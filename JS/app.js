0064421cb788670626e45790e43b5d7bdab1e8fe
andyfiveeleven <fouracesguitar@gmail.com>
2017-06-14 10:48:10 -0700

    folders folders folders for code organization ?

---
+++ JS/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function Project (name, date, tools, path, gitPath, collabs, about){
+  this.name = name;
+  this.date = dateFin;
+  this.tools = tools; //array!
+  this.path = url;
+  this.gitPath = gitHub;
+  this.collabs = collabs;
+  this.about = about;
+}
+
+$('.icon-menu').click(function(){
+  $('.main-nav ul').toggleClass('menu');
+  $('.main-nav ul').toggleClass('display-nav');
+});
+
+$('.main-nav ul').click(function(){
+  $('.main-nav ul').toggleClass('menu');
+  $('.main-nav ul').toggleClass('display-nav');
+});
---
+++ JS/source-data.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var project = [
+  {
+    name:
+    date:
+    tools: //array!
+    path:
+    gitPath:
+    collabs:
+    about:
+  }
+  {
+    name:
+    date:
+    tools: //array!
+    path:
+    gitPath:
+    collabs:
+    about:
+  }
+  {
+    name:
+    date:
+    tools: //array!
+    path:
+    gitPath:
+    collabs:
+    about:
+  }
+]
