b0578229e9fb420516765dce52eca4e70098f1dc
andyfiveeleven <fouracesguitar@gmail.com>
2017-06-16 09:46:42 -0700

    main nav positioning fixed, now over the hero

--- JS/app.js
+++ JS/app.js
@@ -3,6 +3,7 @@
 $('.icon-menu').click(function(){
   $('.main-nav ul').toggleClass('menu');
   $('.main-nav ul').toggleClass('display-nav');
+  $('.icon-menu').toggleClass('fixed');
 });
 
 $('.main-nav ul').click(function(){
