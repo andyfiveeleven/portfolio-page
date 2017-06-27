44a732eae7e8f2b5318bd4c0fd5fe77a2402b1c5
andyfiveeleven <fouracesguitar@gmail.com>
2017-06-27 12:44:48 -0700

    fully loading accordian

--- public/JS/source-data.js
+++ public/JS/source-data.js
@@ -5,7 +5,7 @@ var rawProject = [
     name: 'Salmon Cookies',
     date: 'May 2017',
     tools: 'HTML and CSS',
-    gitPath:'www.gitHub.com',
+    gitPath:'<a href="https://github.com/andyfiveeleven/Salmon-Cookies" target="_blank">GitHub</a>',
     collabs: 'Nickypoo',
     about:'Salmon Cookies',
     shortAbout: 'A Short description of Salmon Cookies!'
@@ -14,7 +14,7 @@ var rawProject = [
     name: 'Chocolate Pizza',
     date: 'May 2017',
     tools: 'HTML, CSS and JavaScript',
-    gitPath:'www.gitHub.com',
+    gitPath:'<a href="https://github.com/andyfiveeleven/Chocolate-Pizza" target="_blank">GitHub</a>',
     collabs:'Nickypoo',
     about: 'Chocolate Pizza',
     shortAbout: 'A Short description of Chocolate Pizza!'
@@ -24,7 +24,7 @@ var rawProject = [
     date: 'May 2017',
     tools: 'HTML, CSS and Javascript',
     path:'www.gitHub.com',
-    gitPath:'www.gitHub.com',
+    gitPath:'<a href="https://github.com/andyfiveeleven/Mix-It-Up" target="_blank">GitHub</a>',
     collabs:'Matty Marky Chrissy',
     about: 'User selects the liquors they have, we output a drink!',
     shortAbout: 'A Short description of Mix It Up!'
