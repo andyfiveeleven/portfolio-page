5155465368081dc0c914a3a13fcaa7e0dd7129a5
andyfiveeleven <fouracesguitar@gmail.com>
2017-06-27 11:23:22 -0700

    added active class toggle instead of slide down, still need to animate the height change

--- public/JS/app.js
+++ public/JS/app.js
@@ -14,10 +14,12 @@ $('.main-nav ul li').click(function(){
 
 $(document).ready(function(){
   $('.accordian h3').click(function(){
-    $('.accordian ul ul').slideUp();
-    if(!$(this).next().is(':visible'))
-    {
-      $(this).next().slideDown();
+    // $('.accordian ul li  ul').removeClass('active');
+    if(!$(this).next().hasClass('active')){
+      $('.accordian ul li  ul').removeClass('active');
+      $(this).next().toggleClass('active');
+    }else{
+      $('.accordian ul li  ul').removeClass('active');
     }
   });
 });
